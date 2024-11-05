b6559d205343436074e5efa9d2a399451b96a620
Kaoweer <kaow.arch@gmail.com>
2024-11-05 13:05:56 +0700

    map decoration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [{
+      mytheme : {
+        "primary" : "#5abd4f",
+        "secondary" : "42b72a",
+        "accent" : "#37cdbe",
+        "neutral" : "#F6F5FA",
+        "base-100" : "#ffffff"
+      }
+    }],
+  },
 }
 
