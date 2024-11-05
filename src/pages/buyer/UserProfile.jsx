d108afd3bea1b37d1f2bd50a0976a452dab6c43c
Tteenimith <tteenimith@gmail.com>
2024-11-05 13:51:54 +0700

    80% profile

--- src/pages/buyer/UserProfile.jsx
+++ src/pages/buyer/UserProfile.jsx
@@ -6,20 +6,34 @@ const UserProfile = () => {
 const [isEdit,setIsEdit] = useState (false)
 const [fName , setFName] = useState("john")
 const [lName , setLName] = useState("conner")
+const [editNumber , setEditNumber] = useState("0859508731")
+const [editAddress , setEditAddress] = useState ("61 ถ.ไทนิยม อ.วังทองหลาง จ.เชียงราย 11920")
+
+// image
+const [file,setFile] = useState(null)
+const [loading, setLoading] = useState(false)
 
 const hdlSubmit = async (e) =>{
     e.preventDefault()
     setIsEdit(!isEdit)
 }
 
-// const hdlChange = async (e) =>{
-//   console.log(e.target.value)
-//   const body = {
-//     [e.target.value]:e.target.value
-//   }
-//   console.log(body)
-//   setFName
-// }
+const hdlImage = async (e) =>{
+  try {
+    setLoading(true)
+    const img = new FormData()
+    if (file) {
+      body.append("image",file)
+    }
+  } catch (err) {
+    console.log(err)
+  }finally{
+    setLoading(false)
+  }
+}
+
+
+
 
   return <div className="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-lg">
   <h1 className="text-xl font-semibold mb-6">{fName} {lName}</h1>
@@ -48,37 +62,63 @@ const hdlSubmit = async (e) =>{
           className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green-500"
         />
         ):(
-          <p>{fName}</p>
+          <p className='w-full px-3 py-2 font-semibold'>{fName}</p>
         )}
 
 
       </div>
       <div>
         <label className="block text-sm font-medium text-gray-700 mb-1">Last name</label>
-        <input 
-          type="text"
-          placeholder="Enter Last Name"
-          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green-500"
-        />
+         {isEdit ?(
+           <input
+           value={lName}
+           onChange={(e)=>setLName(e.target.value)}
+           type="text"
+           placeholder="Enter Last Name"
+           className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green-500"
+         />
+          )
+          :(
+            <p className='w-full px-3 py-2 font-semibold'>{lName}</p>
+          )
+        }
       </div>
     </div>
 
     <div>
       <label className="block text-sm font-medium text-gray-700 mb-1">Phone number</label>
-      <input 
-        type="tel"
-        placeholder="Enter your phone number"
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green-500"
-      />
+      {isEdit ? (
+        <input 
+          value={editNumber}
+          onChange={(e)=>setEditNumber(e.target.value)}
+          type="tel"
+          placeholder="Enter your phone number"
+          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green-500"
+        />
+
+      ) : (
+        <p className=' w-full px-3 py-2'>{editNumber}</p>
+        )}
     </div>
 
     <div>
       <label className="block text-sm font-medium text-gray-700 mb-1">Address</label>
-      <textarea 
-        placeholder="Enter Address"
-        rows={4}
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green-500"
-      />
+      {isEdit
+      ? (
+        <textarea
+          value={editAddress}
+          onChange={setEditAddress} 
+          placeholder="Enter Address"
+          rows={4}
+          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green-500"
+        />
+
+      )
+      :
+      (
+        <p className="w-full px-3 py-2">{editAddress}</p>
+      )
+      }
     </div>
 
     <div className="flex space-x-4">
@@ -86,8 +126,8 @@ const hdlSubmit = async (e) =>{
       type='submit'
       onClick={hdlSubmit}
       >
-      {isEdit ? "Save" : "Edit"  }
-      Profile
+      {isEdit ? "Save" : "Edit"   }
+      
       </button>
       <button className="flex-1 bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 transition-colors">
         Delete Profile
