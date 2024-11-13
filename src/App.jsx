27db16f0bc3691723b781b7741c90f30a54f3d14
Nutchanon Keawhawong <5105nutchanon@gmail.com>
2024-11-13 20:00:49 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import AdminLayout from "./layouts/AdminLayout";
 import { BrowserRouter } from 'react-router-dom';
 import AppRoute from "./routers/AppRoute";
 import useUserStore from "./stores/userStore";
-// import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
 
   return (
     <>
-      {/* <GoogleOAuthProvider clientId={import.meta.env.VITE_API_GOOGLE_CLIENT_ID}> */}
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_API_GOOGLE_CLIENT_ID}>
         <AppRoute />
-      {/* </GoogleOAuthProvider> */}
+      </GoogleOAuthProvider>
     </>
   );
 }
