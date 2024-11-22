41b72577fdd0af1313134cc05112567fd067700a
Nutchanon Keawhawong <5105nutchanon@gmail.com>
2024-11-22 13:06:58 +0700

    Update Interceptor.js

--- src/API/Interceptor.js
+++ src/API/Interceptor.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.REACT_APP_BASE_URL || 'http://localhost:8000', 
+  baseURL: import.meta.env.REACT_APP_BASE_URL || 'https://food-saver-server.onrender.com' 
 });
 
 axiosInstance.interceptors.request.use(
